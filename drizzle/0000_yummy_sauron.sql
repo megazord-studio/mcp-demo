4ba171b99eb2cf946972e20849b72bb972fe0e9d
Benjamin Cirmena <beni@megazord.studio>
2025-08-16 11:05:50 +0200

    add db schema

---
+++ drizzle/0000_yummy_sauron.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "attendees" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"nickname" varchar(255)
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+
+export const attendees = pgTable('attendees', {
+  id: serial('id').primaryKey(),
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  lastName: varchar('last_name', { length: 255 }).notNull(),
+  nickname: varchar('nickname', { length: 255 }),
+});
+
+export type InsertAttendee = typeof attendees.$inferInsert;
+export type SelectAttendee = typeof attendees.$inferSelect;
